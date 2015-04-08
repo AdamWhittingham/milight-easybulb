c61a6703f415d50f0ebe33f78d0fc3879e6e3d52
Adam Whittingham <adam.whittingham@gmail.com>
2015-04-08 23:45:20 +0100

    Add the whole API from docs (brightness might need work!)

--- lib/milight/rgbw.rb
+++ lib/milight/rgbw.rb
@@ -2,8 +2,22 @@ require 'socket'
 
 module Milight
   class RGBW
-    ON   = [0x42, 0x00, 0x55].pack('C*')
-    OFF  = [0x41, 0x00, 0x55].pack('C*')
+    OFF  = 0x41
+    ON   = 0x42
+
+    GROUP_ON = [0x45, 0x47, 0x49, 0x4B]
+    GROUP_OFF = [0x46, 0x48, 0x4A, 0x4C]
+    GROUP_ALL = 0x42
+
+    GROUP_WHITE = [0xC5, 0xC7, 0xC9, 0xCB]
+    WHITE_ALL = 0xC2
+
+    DISCO_MODE = 0x4D
+    DISCO_SLOWER = 0x43
+    DISCO_FASTER = 0x44
+
+    BRIGHTNESS_CMD = 0x4E
+    COLOUR_CMD = 0x40
 
     def initialize(ip_address, port=8899)
       @socket = UDPSocket.new
@@ -12,11 +26,84 @@ module Milight
     end
 
     def on
-      @socket.send ON, 0, @ip_address, @port
+      send_command ON
     end
 
     def off
-      @socket.send OFF, 0, @ip_address, @port
+      send_command OFF
     end
+
+    def group_on group
+      raise invalid_group_error unless valid_group?(group)
+      send_command GROUP_ON[group - 1]
+    end
+
+    def group_off group
+      raise invalid_group_error unless valid_group?(group)
+      send_command GROUP_OFF[group - 1]
+    end
+
+    def select_group group
+      group_on group
+      sleep 0.1
+    end
+
+    def select_all
+      send_command GROUP_ALL
+      sleep 0.1
+    end
+
+    def brightness percent
+      raise invalid_percent_error unless valid_percent?(percent)
+      level = 59 * percent / 100
+      send_command BRIGHTNESS_CMD, level
+    end
+
+    def colour colour
+      raise invalid_colour_error unless valid_colour?(colour)
+      send_command COLOUR_CMD, colour
+    end
+
+    def all_white
+      select_all
+      send_command WHITE_ALL
+    end
+
+    def group_white group
+      raise invalid_group_error unless valid_group?(group)
+      select_group group
+      send_command GROUP_WHITE[group - 1]
+    end
+
+    private
+
+    def send_command cmd, arg1=0x00
+      @socket.send [cmd, arg1, 0x55].pack('C*'), 0, @ip_address, @port
+    end
+
+    def invalid_group_error
+      ArgumentError.new('Group must be between 1 and 4')
+    end
+
+    def valid_group? value
+      value.between?(1,4)
+    end
+
+    def invalid_percent_error
+      ArgumentError.new('Percentages are generally between 0 and 100')
+    end
+
+    def valid_colour? value
+      value.between?(0,255)
+    end
+
+    def invalid_colour_error
+      ArgumentError.new('Colours must be between 0 and 255')
+    end
+
+    def valid_percent? value
+      value.between?(0,100)
+    end
+
   end
 end
