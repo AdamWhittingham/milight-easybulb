418b9f05d172a8e83431e54cab608b556a8f26fc
Adam Whittingham <adam.whittingham@gmail.com>
2015-04-20 23:24:51 +0100

    Add RGB and HSL colour helpers

--- lib/milight/colour.rb
+++ lib/milight/colour.rb
@@ -6,7 +6,7 @@ module Milight
       magenta:    180,
       orange:     155,
       yellow:     140,
-      green:      100,
+      green:       85,
       lime:       120,
       teal:        70,
       blue:        50,
@@ -28,7 +28,7 @@ module Milight
     end
 
     def colour_named name
-      name_sym = name.downcase.to_sym
+      name_sym = name.to_s.downcase.gsub(/ +/,'_').to_sym
       raise invalid_colour_name_error(name) unless valid_name? name_sym
       NAMED_COLOURS[name_sym]
     end
@@ -38,7 +38,58 @@ module Milight
       number
     end
 
+    def rgb r, g, b
+      h = rgb_to_hsl(r,g,b).first
+      hsl(h, 1.0, 0.5)
+    end
+
+    def hsl h,s,l
+      mod = (h / 120) *  50
+      (h + 170 + mod) % 255
+    end
+
+    def rgb_to_hsl(r, g, b)
+      r /= 255.0
+      g /= 255.0
+      b /= 255.0
+      h = hue(r,g,b)
+      l = luminosity(r,g,b)
+      s = saturation(r,g,b,l)
+      [h,s,l]
+    end
+
+    def greyscale? r, g, b
+      (r == g && g == b)
+    end
+
     private
+
+    def delta r,g,b
+      [r,g,b].minmax.reverse.inject(:-)
+    end
+
+    def hue r, g, b
+      return 0 if greyscale?(r,g,b)
+      delta = delta(r,g,b)
+      offset = case [r,g,b].max
+      when r
+        ((g - b)/delta)
+      when g
+        ((b - r)/delta) + 2
+      when b
+        ((r - g)/delta) + 4
+      end
+      60 * offset % 360
+    end
+
+    def saturation r,g,b, l
+      return 0 if greyscale?(r,g,b)
+      delta(r,g,b) / 1 - (2 * l - 1).abs
+    end
+
+    def luminosity r,g,b
+      [r,g,b].minmax.inject(:+) / 2
+    end
 
     def invalid_colour_error
       ArgumentError.new('Colours must be a symbol or a number between 0 and 255')
--- spec/milight/colour_spec.rb
+++ spec/milight/colour_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 require production_code
 
 describe Milight::Colour do
-  describe '.of' do
+  let(:red_code)   { 170 }
+  let(:green_code) { 85 }
+  let(:blue_code)  { 0 }
+
+  describe '#of' do
     it 'returns a valid colour number' do
       expect(subject.of 255).to eq 255
     end
@@ -12,11 +16,11 @@ describe Milight::Colour do
     end
 
     it 'takes a symbol for named colours' do
-      expect(subject.of :blue).to eq 50
+      expect(subject.of :dark_blue).to eq blue_code
     end
 
     it 'takes a string for named colours' do
-      expect(subject.of 'Blue').to eq 50
+      expect(subject.of 'Dark Blue').to eq blue_code
     end
 
     it 'raises an exception for invalid names' do
@@ -25,6 +29,49 @@ describe Milight::Colour do
 
     it 'balks at arguments which are not a valid number or name' do
       expect{subject.of 0.2}.to raise_error ArgumentError
+    end
+  end
+
+  describe '#rgb' do
+    it 'returns the milight colour code for the given RGB values' do
+      expect(subject.rgb 255,   0,   0).to eq red_code
+      expect(subject.rgb 0,   255,   0).to eq green_code
+      expect(subject.rgb 0,     0, 255).to eq blue_code
+    end
+  end
+
+  describe '#hsl' do
+    it 'returns the milight colour code for the given HSL values' do
+      expect(subject.hsl   0, 1.0, 0.5).to eq red_code
+      expect(subject.hsl 120, 1.0, 0.5).to eq green_code
+      expect(subject.hsl 240, 1.0, 0.5).to eq blue_code
+    end
+  end
+
+  describe '#rgb_to_hsl' do
+    it 'converts the RGB colour to HSL' do
+      expect(subject.rgb_to_hsl(255,   0,   0)).to eq [0,1.0,0.5]
+      expect(subject.rgb_to_hsl(  0, 255,   0)).to eq [120,1.0,0.5]
+      expect(subject.rgb_to_hsl(  0,   0, 255)).to eq [240,1.0,0.5]
+      expect(subject.rgb_to_hsl(255,   0, 255)).to eq [300,1.0,0.5]
+    end
+  end
+
+  describe '#greyscale?' do
+    it 'is true for greys' do
+      expect(subject.greyscale?(10, 10, 10)).to eq true
+    end
+
+    it 'is true for white' do
+      expect(subject.greyscale?(255, 255, 255)).to eq true
+    end
+
+    it 'is true for black' do
+      expect(subject.greyscale?(0, 0, 0)).to eq true
+    end
+
+    it 'is false for colours' do
+      expect(subject.greyscale?(0, 100, 0)).to eq false
     end
   end
 end
