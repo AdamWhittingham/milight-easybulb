8894a2f8a5dfea671d4d85042a9919f40a16950a
Adam Whittingham <adam@devmountain.co.uk>
2015-08-30 17:37:46 +0100

    Merge Colour::HSL into Colour

--- lib/milight/colour.rb
+++ lib/milight/colour.rb
@@ -1,26 +1,32 @@
-require_relative 'colour/hsl'
 require_relative 'brightness'
 
 module Milight
   class Colour
+    attr_reader :hue, :saturation, :luminosity, :red, :green, :blue
+
     HUE_OFFSET = 170
+    VALID_HEX_REGEX = /^#?([0-9a-f]{3}){,2}$/i
 
     def initialize(value)
-      @colour = case value
-                when String
-                  Milight::Colour::HSL.new.from_hex(value)
-                when Array
-                  Milight::Colour::HSL.new.from_rgb(*value)
-                else
-                  raise invalid_colour_error
-                end
-      self
+      case value
+      when String then from_hex(value)
+      when Array  then from_rgb(*value)
+      else
+        raise invalid_colour_error
+      end
+    end
+
+    def to_hsl
+      [@hue, @saturation, @luminosity]
+    end
+
+    def to_rgb
+      [@red, @green, @blue]
     end
 
     def to_milight_colour
-      hue = @colour.hue
-      mod = (hue / 120) * 50
-      (hue + HUE_OFFSET + mod).round % 255
+      mod = (@hue / 120) * 50
+      (@hue + HUE_OFFSET + mod).round % 255
     end
 
     def to_milight_brightness
@@ -29,18 +35,78 @@ module Milight
     end
 
     def greyscale?
-      @colour.greyscale?(@colour.red, @colour.green, @colour.blue)
+      @red == @green && @red == @blue
     end
 
     private
 
+    def from_rgb(r, g, b)
+      @red, @green, @blue = r, g, b
+      @hue, @saturation, @luminosity = rgb_to_hsl(r, g, b)
+    end
+
+    def from_hex(hex)
+      raise invalid_hex_colour_error unless valid_hex_colour? hex
+      r,g,b = hex_to_rgb(hex)
+      from_rgb(r,g,b)
+    end
+
     def brightness_for_saturation
-      r,g,b = *@colour.to_rgb
+      r,g,b = *to_rgb
       (Math.sqrt(r*r + g*g + b*b) / 2.55 ).round
     end
 
+    def hex_to_rgb(hex)
+      hex.sub('#', '')
+        .chars
+        .each_slice(hex.length / 3)
+        .map{|val| val.length == 1 ? val * 2 : val }
+        .map(&:join)
+        .map { |h| h.to_i(16) }
+    end
+
+    def rgb_to_hsl(r, g, b)
+      r,g,b = [r,g,b].map!{|c| c /= 255.0 }
+      h = rgb_to_hue(r, g, b)
+      l = rgb_to_luminosity(r, g, b)
+      s = rgbl_to_saturation(r, g, b, l)
+      [h, s, l]
+    end
+
+    def rgb_to_hue(r, g, b)
+      return 0 if greyscale?
+      delta = delta(r, g, b)
+      offset = case [r, g, b].max
+               when r then ((g - b) / delta)
+               when g then ((b - r) / delta) + 2
+               when b then ((r - g) / delta) + 4
+               end
+      60 * offset % 360
+    end
+
+    def rgb_to_luminosity(r, g, b)
+      [r, g, b].minmax.inject(:+) / 2
+    end
+
+    def rgbl_to_saturation(r, g, b, l)
+      return 0 if greyscale?
+      delta(r, g, b) / 1 - (2 * l - 1).abs
+    end
+
+    def delta(r, g, b)
+      [r, g, b].minmax.reverse.inject(:-)
+    end
+
+    def valid_hex_colour?(value)
+      value =~ VALID_HEX_REGEX
+    end
+
     def invalid_colour_error
-      ArgumentError.new('Colours must be given as with a hex colour string or a RGB array: #{described_class}.new([r,g,b])')
+      ArgumentError.new("Colours must be given as with a hex colour string ( #{described_class}.new('#foo') ) or a RGB array ( #{described_class}.new([r,g,b]) )")
+    end
+
+    def invalid_hex_colour_error
+      ArgumentError.new('Hex colours codes must be 3 or 6 0-9 or a-f characters')
     end
 
   end
--- spec/milight/colour_spec.rb
+++ spec/milight/colour_spec.rb
@@ -1,18 +1,45 @@
 require 'spec_helper'
+require 'ostruct'
 require production_code
 
 describe Milight::Colour do
-  let(:red_code)   { 170 }
-  let(:green_code) { 85 }
-  let(:blue_code)  { 0 }
+
+  let(:red)   { OpenStruct.new(milight: 170, rgb: [255,   0,   0], hsl:[  0.0, 1.0, 0.5]) }
+  let(:green) { OpenStruct.new(milight:  85, rgb: [  0, 255,   0], hsl:[120.0, 1.0, 0.5]) }
+  let(:blue)  { OpenStruct.new(milight:   0, rgb: [  0,   0, 255], hsl:[240.0, 1.0, 0.5]) }
+
+  describe '#new' do
+    it 'takes valid 6-digit hex codes' do
+      expect(described_class.new('#FF0000').to_hsl).to eq red.hsl
+    end
+
+    it 'takes valid 3-digit hex hsls' do
+      expect(described_class.new('#0F0').to_hsl).to eq green.hsl
+    end
+
+    it 'takes valid hex hsls without a leading hash' do
+      expect(described_class.new('0F0'   ).to_hsl).to eq green.hsl
+      expect(described_class.new('00ff00').to_hsl).to eq green.hsl
+    end
+
+    it 'raises an arguement error for invalid hex codes' do
+      expect { described_class.new('#00112233') }.to raise_error ArgumentError
+    end
+
+    it 'takes an RGB array' do
+      expect(described_class.new([255,   0,   0]).to_hsl).to eq red.hsl
+      expect(described_class.new([0,   255,   0]).to_hsl).to eq green.hsl
+      expect(described_class.new([0,     0, 255]).to_hsl).to eq blue.hsl
+    end
+  end
 
   describe '#to_milight_colour' do
     it 'converts hex codes to milight colour codes' do
-      expect(described_class.new('#FF0000').to_milight_colour ).to eq red_code
+      expect(described_class.new('#FF0000').to_milight_colour ).to eq red.milight
     end
 
     it 'converts an array of RGB values to milight colour codes' do
-      expect(described_class.new( [255, 0 ,0 ]).to_milight_colour ).to eq red_code
+      expect(described_class.new( [255, 0 ,0 ]).to_milight_colour ).to eq red.milight
     end
 
   end
@@ -22,6 +49,22 @@ describe Milight::Colour do
       expect(described_class.new('#00FF00').to_milight_brightness ).to eq 27
       expect(described_class.new('#008800').to_milight_brightness ).to eq 15
       expect(described_class.new('#000000').to_milight_brightness ).to eq  2
+    end
+  end
+
+  describe '#to_rgb' do
+    it 'outputs an array of RGB' do
+      expect(described_class.new('#FF0000').to_rgb ).to eq red.rgb
+      expect(described_class.new('#00FF00').to_rgb ).to eq green.rgb
+      expect(described_class.new('#0000FF').to_rgb ).to eq blue.rgb
+    end
+  end
+
+  describe '#to_hsl' do
+    it 'outputs an array of HSL' do
+      expect(described_class.new('#FF0000').to_hsl ).to eq red.hsl
+      expect(described_class.new('#00FF00').to_hsl ).to eq green.hsl
+      expect(described_class.new('#0000FF').to_hsl ).to eq blue.hsl
     end
   end
 
